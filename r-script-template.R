716762042255af64c5f927bc950f84b36381b8ea
Phillip Hungerford <37560298+philliphungerford@users.noreply.github.com>
2022-03-25 14:17:18 +1100

    Update r-script-template.R
    Added library loader

--- r-script-template.R
+++ r-script-template.R
@@ -21,6 +21,26 @@ options(scipen = 6, digits = 4)
 # require(tidyverse)
 # require(data.table)
 # source("functions/packages.R")       # loads up all the packages we need
+
+# Alternatively for packages
+fLoadPackages <- function(...) {
+	
+	#'''
+	# Purpose: Loads packages for user, if package not detected will install 
+	# Inputs: List of packages
+	# Outputs: Nothing
+	# Example: fLoadPackages('odbc','RODBC','DBI','tidyverse')
+	# '''
+	libs <- unlist(list(...))
+    req <- unlist(lapply(X = libs, FUN = require, character.only=TRUE))
+    need <- libs[req==FALSE]
+    
+    if(length(need)>0){ 
+        install.packages(need)
+        lapply(need,require,character.only=TRUE)
+    }
+}
+
 ################################################################################
 # Level 1
 #===============================================================================
@@ -34,17 +54,17 @@ options(scipen = 6, digits = 4)
 #===============================================================================
 fExampleFunction() <- function(){
     
-	#'''
-    # Purpose:
+    #'''
+	# Purpose:
 	# Inputs:
 	# Outputs:
 	# Example: 
-    # '''
+	# '''
 	
 	#---------------------------------------------------------------------------
 	# Step 1
 	
-    #---------------------------------------------------------------------------
+	#---------------------------------------------------------------------------
 	# Step 2
 	
 	#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
